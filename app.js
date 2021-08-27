7c889e9bf8d403cbe0c5c05442940be9df320490
maurya512 <patelmaurya0512@gmail.com>
2021-08-26 23:34:39 -0400

    established connection to mongodb

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 // bring in express
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 // initiliaze the app
 const app = express();
 
+// database config  by bringing in the mongouri from config folder
+const DB = require('./config/keys').mongoURI;
+
+// connection to mongodb
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
 // ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
 
 // setup a port for deployment
 const PORT = process.env.PORT || 5000;
